df8a3bc0a43b05c1ef529a28f616f620b093f825
Tim-Oliver Husser <thusser@uni-goettingen.de>
2022-06-28 14:44:39 +0200

    working on wrapper

--- pyobs_flipro/fliprodriver.pyx
+++ pyobs_flipro/fliprodriver.pyx
@@ -10,8 +10,20 @@ np.import_array()
 
 from libflipro cimport *
 
+
+cdef class DeviceInfo:
+    cdef FPRODEVICEINFO obj
+    def __init__(self, obj):
+        self.obj = obj
+    def __getattr__(self, item):
+        return getattr(self.obj, item)
+
+
 cdef class FliProDriver:
     """Wrapper for the FLI driver."""
+
+    cdef FPRODEVICEINFO _device_info
+    cdef int32_t _handle
 
     @staticmethod
     def get_api_version() -> str:
@@ -20,3 +32,26 @@ cdef class FliProDriver:
         cdef uint32_t length = 100
 
         success = FPROCam_GetAPIVersion(version, length)
+
+    @staticmethod
+    def list_devices() -> List[DeviceInfo]:
+        cdef LIBFLIPRO_API success
+        cdef FPRODEVICEINFO pDeviceInfo[10]
+        cdef uint32_t pNumDevices = 10
+
+        success = FPROCam_GetCameraList(pDeviceInfo, &pNumDevices)
+
+        devices = [DeviceInfo(pDeviceInfo[i]) for i in range(pNumDevices)]
+        return devices
+
+    def __init__(self, device_info: DeviceInfo):
+        self._device_info = device_info.obj
+        self._handle = 0
+
+    def open(self):
+        cdef LIBFLIPRO_API success
+        success = FPROCam_Open(&self._device_info, &self._handle)
+
+    def close(self):
+        cdef LIBFLIPRO_API success
+        success = FPROCam_Close(self._handle)
--- pyobs_flipro/libflipro.pxd
+++ pyobs_flipro/libflipro.pxd
@@ -1,8 +1,23 @@
 from libc.stdint cimport int32_t, uint32_t
 from libc.stddef cimport wchar_t
 
-
 cdef extern from "../lib/libflipro.h":
     ctypedef int32_t LIBFLIPRO_API
 
-    LIBFLIPRO_API FPROCam_GetAPIVersion(wchar_t *pVersion, uint32_t uiLength);
+    ctypedef enum FPROCONNECTION: FPRO_CONNECTION_USB, FPRO_CONNECTION_FIBRE
+    ctypedef enum FPROUSBSPEED: FPRO_USB_FULLSPEED, FPRO_USB_HIGHSPEED, FPRO_USB_SUPERSPEED
+
+    ctypedef struct FPRODEVICEINFO:
+        wchar_t cFriendlyName[256]
+        wchar_t cSerialNo[256]
+        wchar_t cDevicePath[1024]
+        FPROCONNECTION eConnType
+        uint32_t uiVendorId
+        uint32_t uiProdId
+        FPROUSBSPEED eUSBSpeed
+
+    LIBFLIPRO_API FPROCam_GetAPIVersion(wchar_t *pVersion, uint32_t uiLength)
+    LIBFLIPRO_API FPROCam_GetCameraList(FPRODEVICEINFO *pDeviceInfo, uint32_t *pNumDevices)
+    LIBFLIPRO_API FPROCam_Open(FPRODEVICEINFO *pDevInfo, int32_t *pHandle)
+    LIBFLIPRO_API FPROCam_Close(int32_t iHandle)
+
